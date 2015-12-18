01bb3a91681918a608a05cafc56a5a06f93925e6
adam <adam.amitai@gmail.com>
2015-12-18 05:43:42 +0200

    new explanation for the test mode

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import Chat
 from ExternalServer import ExternalServer
 
+###########################################
 TEST = True
+# in this case run in two different consoles
+# on the same machine first time
+# python main.py a
+# second time
+# python main.py b
+###########################################
 
 external_server = ExternalServer()
 chatRef = Chat.Chat(external_server)
